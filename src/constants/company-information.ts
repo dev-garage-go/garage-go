f55e4c227c5ad839e05c20c0b5f1861c610bfab8
Alex Arraga <alex@MacBook-Pro-de-Jorge.local>
2025-04-22 12:05:47 -0300

    Company information added

---
+++ src/constants/company-information.ts
@@ -0,0 +1,11 @@
+// Data
+export const CompanyName = "Garage Go"
+export const CompanyDescription = "El mejor servicio automotriz al precio más conveniente. Reserva ahora tu servicio y vamos a buscar y a dejar tu vehículo a domicilio"
+export const CompanyHoursOperations = "Lun-Vie de 8:30 a 18:15"
+
+// Contact
+export const CompanyWhatsappLink = "https://wa.me/56997832409";
+export const CompanyWhatsappNumber = 56997832409;
+export const CompanyEmail = ""
+export const CompanyInstagram = "https://www.instagram.com/garagego.cl/"
+export const CompanyFacebook = "https://www.facebook.com/garagego.cl"
