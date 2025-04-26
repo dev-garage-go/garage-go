a5d53b587c7fd57fe7dae50561549eb54c7176c7
Alex Arraga <arraga.alex@gmail.com>
2025-04-26 00:55:15 -0300

    banner improved

--- src/components/home/Banner/BannerCard.tsx
+++ src/components/home/Banner/BannerCard.tsx
@@ -1,23 +1,31 @@
-import Image from 'next/image'
-import { AgendaCotiza } from '@/assets'
-import React from 'react'
+'use client'
 
+import Image from 'next/image'
 interface Props {
-  title: string,
-  description: string,
+  title: string
+  description: string
   image: string
-  imageAlt: string,
-  hasButton?: boolean,
-  buttonString?: string,
+  imageAlt: string
+  hasButton?: boolean
+  buttonString?: string
   buttonStyle?: string
   buttonFunc?: () => void
 }
 
-export const BannerCard = ({ description, image, title, imageAlt, hasButton, buttonString, buttonStyle, buttonFunc }: Props) => {
+export const BannerCard = ({
+  description,
+  image,
+  title,
+  imageAlt,
+  hasButton = false,
+  buttonString,
+  buttonStyle,
+  buttonFunc = () => { }
+}: Props) => {
   return (
     <div className='flex justify-center items-center py-8 md:py-10 xl:py-12'>
       <div className='relative flex items-center w-full max-w-5xl h-72 md:h-96 bg-primaryBlue-900 rounded-3xl shadow-xl shadow-gray-400 overflow-hidden p-6 md:px-10 md:py-12'>
-        {/* Texto */}
+        {/* Title and description */}
         <div className={`relative w-full sm:w-1/2 text-white flex flex-col justify-center ${hasButton ? 'gap-10' : 'gap-0'}`}>
           <div>
             <h2 className='text-lg md:text-2xl font-semibold mb-4'>{title}</h2>
@@ -26,18 +34,19 @@ export const BannerCard = ({ description, image, title, imageAlt, hasButton, but
             </p>
           </div>
           {
-            hasButton ? (
+            hasButton && (
               <button
+                type='button'
                 onClick={buttonFunc}
-                className={`font-medium py-2 text-sm sm:text-base px-10 w-full rounded-md ${buttonStyle}`}
+                className={`font-medium py-2 text-sm sm:text-base w-full rounded-md ${buttonStyle}`}
               >
                 {buttonString}
               </button>
-            ) : ''
+            )
           }
         </div>
 
-        {/* Imagen: posicionada dentro del contenedor azul */}
+        {/* Image */}
         <div className='hidden sm:block absolute bottom-0 right-0 h-full w-1/2'>
           <Image
             priority
