f7881d6a902941fbdbe498d63fc268e98dc3625f
Alex Arraga <arraga.alex@gmail.com>
2025-04-28 18:36:44 -0300

    tires change page added

--- src/app/(shop)/services/tire_change/page.tsx
+++ src/app/(shop)/services/tire_change/page.tsx
@@ -1,13 +1,71 @@
-import { redirect } from "next/navigation";
+import { FAQsDisclosure, OtherServices, ServicePageButtons, StepsCard, TopBanner } from '@/components';
+import { StepsTechnicalRevision, TechnicalRevisionFAQs } from '@/constants';
+import { Mantencion } from '@/assets';
 
-export default function TireChangePage() {
-  // redirect() is a guardian to protect the page, 
-  // deletes this when the page or section is built.
-  redirect("/")
-
+export default function TiresChangePage() {
   return (
     <div>
-      <h1>Hello TireChange Page</h1>
+      {/* Top banner */}
+      <TopBanner
+        title='CAMBIO DE NEUMÁTICOS'
+        description='Cotiza tus neumáticos y obtén sin costo Instalación y balanceo.'
+        withImage
+        imageSrc={Mantencion}
+        imageAlt='playero'
+        featuresImages={['pick-delivery', 'super-check', 'garantia']}
+      />
+
+      <div className='container-section'>
+        <h3 className='title-h3 font-light text-center'>
+          Solicita tu cotización, contamos con amplia variedad
+          <br />
+          Marcas y todas las medidas.
+        </h3>
+
+        {/* Buttons */}
+        <ServicePageButtons />
+      </div>
+
+      <section className='container-section'>
+        <h2 className='title-h2 font-medium'>¿Como funciona?</h2>
+        <p className='description-of-title-h2'>Cotiza, confirma y nos encargaremos de todo</p>
+
+        <div className='max-w-page padding-central-page grid-steps-central-page'>
+          {StepsTechnicalRevision.map((item, index) => (
+            <StepsCard {...item} key={index + item.title} />
+          ))}
+        </div>
+      </section>
+
+      {/* TODO: Benefits and promotions section */}
+
+
+      {/* Title and FAQs section */}
+      <section className='container-section max-w-page w-full'>
+        <h2 className='title-h2 font-medium text-center'>
+          ¿Qué debes saber sobre tu
+          <br />
+          Cambio de neumaticos?
+        </h2>
+
+        <div className='faqs-services-pages'>
+          {TechnicalRevisionFAQs.map((faq, index) => (
+            <FAQsDisclosure {...faq} key={index + faq.question} />
+          ))}
+        </div>
+
+        <div className='flex justify-center items-center w-full mb-10 mt-4'>
+          <button className='text-sm sm:text-base bg-primaryBlue-900 rounded-md px-10 sm:px-14 md:px-20 py-2 text-center text-white'>
+            Agenda Ahora
+          </button>
+        </div>
+      </section>
+
+      {/* Gray section */}
+      <OtherServices
+        searchServicesFrom={5}
+        searchServicesTo={7}
+      />
     </div>
   );
 }
