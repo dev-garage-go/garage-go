849c269acac93fea99b7703a9ebecbe9fb39fb65
Alex Arraga <arraga.alex@gmail.com>
2025-04-28 16:39:05 -0300

    responsive design improved

--- src/app/(shop)/services/mileage_maintenance/page.tsx
+++ src/app/(shop)/services/mileage_maintenance/page.tsx
@@ -1,11 +1,10 @@
-import { FAQsDisclosure, OtherServices, ServiceCard, ServicePageButtons, StepsCard, TopBanner } from '@/components';
-import { MileageMaintenanceFAQs, ServicesData, StepsMileageMaintenance } from '@/constants';
+import { FAQsDisclosure, OtherServices, ServicePageButtons, StepsCard, TopBanner } from '@/components';
+import { MileageMaintenanceFAQs, StepsMileageMaintenance } from '@/constants';
 import { Mantencion } from '@/assets';
-import Link from 'next/link';
 
 export default function MileageMaintenancePage() {
   return (
-    <div className='min-h-screen'>
+    <div>
       {/* Top banner */}
       <TopBanner
         title='MANTENCIÓN POR KILOMETRAJE'
@@ -16,7 +15,7 @@ export default function MileageMaintenancePage() {
         featuresImages={['pick-delivery', 'super-check', 'garantia']}
       />
 
-      <div className='flex flex-col justify-center gap-10 items-center w-full mt-20'>
+      <div className='container-section'>
         <h3 className='title-h3 text-center'>
           Reserva tu mantención por kilometraje desde los $189.990
           <br />
@@ -25,46 +24,45 @@ export default function MileageMaintenancePage() {
 
         {/* Buttons */}
         <ServicePageButtons />
-
-        <section className='container-section'>
-          <h2 className='title-h2 font-semibold'>¿Como funciona?</h2>
-          <p className='description-of-title-h2'>Darle el mejor cuidado a tu auto es muy facil</p>
-
-          <div className='max-w-page padding-central-page grid-steps-central-page'>
-            {StepsMileageMaintenance.map((item, index) => (
-              <StepsCard {...item} key={index} />
-            ))}
-          </div>
-        </section>
-
-        {/* Title and FAQs section */}
-        <section className='container-section max-w-page w-full'>
-          <h2 className='title-h2 font-semibold text-center'>
-            ¿Qué debes saber sobre tu
-            <br />
-            Mantencion por Kilometraje?
-          </h2>
-
-          <div className='space-y-4 w-full sm:w-2/4 mt-6'>
-            {MileageMaintenanceFAQs.map((faq, index) => (
-              <FAQsDisclosure {...faq} key={index} />
-            ))}
-
-          </div>
-
-          <div className='flex justify-center items-center w-full mb-10'>
-            <button className='bg-primaryBlue-900 rounded-md px-20 py-2 text-center text-white'>
-              Agenda Ahora
-            </button>
-          </div>
-        </section>
-
-        {/* Gray section */}
-        <OtherServices
-          searchServicesFrom={3}
-          searchServicesTo={5}
-        />
       </div>
+
+      <section className='container-section'>
+        <h2 className='title-h2 font-semibold'>¿Como funciona?</h2>
+        <p className='description-of-title-h2'>Darle el mejor cuidado a tu auto es muy facil</p>
+
+        <div className='max-w-page padding-central-page grid-steps-central-page'>
+          {StepsMileageMaintenance.map((item, index) => (
+            <StepsCard {...item} key={index} />
+          ))}
+        </div>
+      </section>
+
+      {/* Title and FAQs section */}
+      <section className='container-section max-w-page w-full'>
+        <h2 className='title-h2 font-semibold text-center'>
+          ¿Qué debes saber sobre tu
+          <br />
+          Mantencion por Kilometraje?
+        </h2>
+
+        <div className='space-y-4 w-full lg:w-2/4 mt-6 px-6 sm:px-10 md:px-20 lg:px-0'>
+          {MileageMaintenanceFAQs.map((faq, index) => (
+            <FAQsDisclosure {...faq} key={index} />
+          ))}
+        </div>
+
+        <div className='flex justify-center items-center w-full mb-10 mt-4'>
+          <button className='text-sm sm:text-base bg-primaryBlue-900 rounded-md px-10 sm:px-14 md:px-20 py-2 text-center text-white'>
+            Agenda Ahora
+          </button>
+        </div>
+      </section>
+
+      {/* Gray section */}
+      <OtherServices
+        searchServicesFrom={3}
+        searchServicesTo={5}
+      />
     </div>
   );
 }
--- src/components/Buttons/ServicePageButtons.tsx
+++ src/components/Buttons/ServicePageButtons.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 export const ServicePageButtons = () => {
-  const baseClassBtn = 'flex justify-center items-center text-center py-2 px-10 bg-white rounded-md border hover:text-white duration-300'
+  const baseClassBtn = 'flex justify-center text-sm sm:text-base items-center text-center py-2 px-6 sm:px-8 md:px-10 bg-white rounded-md border hover:text-white duration-300'
+
   return (
-    <div className='flex justify-center items-center gap-6 w-full'>
+    <div className='flex sm:flex-row justify-center items-center gap-4 sm:gap-6 w-full mt-6'>
       <button
         type='button'
         className={`border-green-500 text-green-500 hover:bg-green-600 ${baseClassBtn}`}>
