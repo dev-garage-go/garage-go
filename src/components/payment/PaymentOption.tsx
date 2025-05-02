404f3efc4ebff9874e562b130334e50de90e1fc6
Alex Arraga <arraga.alex@gmail.com>
2025-05-02 02:07:40 -0300

    responsive payment option

--- src/components/payment/PaymentOption.tsx
+++ src/components/payment/PaymentOption.tsx
@@ -18,11 +18,11 @@ export const PaymentOption = ({ method, name, description, imageSrc, checked, re
     <button
       type="button"
       onClick={onClick}
-      className={`flex gap-4 cursor-pointer bg-white w-full border border-gray-300 shadow-md px-4 rounded-2xl 
+      className={`flex gap-4 cursor-pointer bg-white w-full border border-gray-300 shadow-md px-4 py-2 rounded-2xl 
         hover:scale-105 duration-300 transition-all ${checked ? "border-primaryBlue-400 bg-blue-50" : "border-gray-300 "}`
       }>
 
-      <div className="relative flex h-20 w-28 items-center">
+      <div className="relative flex h-16 w-24 md:h-20 md:w-28 items-center">
         <Image
           src={imageSrc}
           alt={name}
@@ -33,14 +33,14 @@ export const PaymentOption = ({ method, name, description, imageSrc, checked, re
 
       <div className="flex w-full justify-between items-center">
         {/* Text */}
-        <div className="flex flex-col items-start justify-center">
-          <h4 className="text-base font-semibold text-blue-900">{name}</h4>
-          <p className="text-sm text-customGray-500">{description}</p>
+        <div className="flex flex-col items-start justify-start gap-1">
+          <h4 className="text-sm md:text-base text-start font-semibold text-blue-900">{name}</h4>
+          <p className="text-xs md:text-sm text-start text-customGray-500">{description}</p>
         </div>
 
         {/* Custom input radio */}
         <div>
-          <div className={`w-5 h-5 rounded-full border-4 
+          <div className={`hidden md:w-5 md:h-5 rounded-full border-4 
             ${checked ? "bg-primaryBlue-400 border-primaryBlue-400" : "border-primaryBlue-900"}`}
           />
 
