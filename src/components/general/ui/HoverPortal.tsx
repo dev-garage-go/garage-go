b26a661365c31d9b9d7a8940753f7a4951576a2b
Alex Arraga <arraga.alex@gmail.com>
2025-05-05 00:15:00 -0300

    wip

--- src/components/general/ui/HoverPortal.tsx
+++ src/components/general/ui/HoverPortal.tsx
@@ -43,7 +43,7 @@ export const HoverPortal = ({ children, anchorRef }: Props) => {
       initial={{ opacity: 0, y: -5 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -5 }}
-      transition={{ duration: 0.2 }}
+      transition={{ duration: 0.4 }}
       style={{
         position: 'absolute',
         top: position.top,
