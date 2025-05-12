d45df6b72d3084f93dedef9ac1b16c39a1aafbc8
Alex Arraga <arraga.alex@gmail.com>
2025-05-12 20:16:21 -0300

    button href fixed

--- src/components/general/buttons/BookingServiceButton.tsx
+++ src/components/general/buttons/BookingServiceButton.tsx
@@ -9,7 +9,7 @@ export const BookingServiceButton = () => {
 
   return (
     <Link
-      href={`${lastPath}/checkout`}
+      href={`${lastPath}/contracting`}
       className='text-sm sm:text-base font-light text-white bg-primaryBlue-900 hover:scale-105 hover:brightness-125 transition-all duration-300 rounded-md px-10 sm:px-14 md:px-28 py-2 text-center'>
       Agenda ahora
     </Link>
