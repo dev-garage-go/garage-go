df773d9f1aa6201ab71247cc9438cab466bdf5a0
Alex Arraga <arraga.alex@gmail.com>
2025-05-15 19:48:01 -0300

    wip

--- src/interfaces/forms/mileage-contractic.ts
+++ src/interfaces/forms/mileage-contractic.ts
@@ -1,5 +1,21 @@
-import { VehicleMileages } from "./vehicle-mileages";
 import { ExtraServices } from '../components-interfaces/extra-services';
+
+export type VehicleMileages = (
+  '10.000 kms' |
+  '20.000 kms' |
+  '30.000 kms' |
+  '40.000 kms' |
+  '50.000 kms' |
+  '60.000 kms' |
+  '70.000 kms' |
+  '80.000 kms' |
+  '90.000 kms' |
+  'Otro'
+)
+
+export interface VehicleMileagesOptions {
+  quantity: VehicleMileages
+}
 
 export interface MileageContractingForm {
   mileages: VehicleMileages
