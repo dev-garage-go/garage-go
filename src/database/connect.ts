098202b68acbb72d74d982480356f1b05634b36b
Alex Arraga <arraga.alex@gmail.com>
2025-05-15 19:03:49 -0300

    wip

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -6,6 +6,16 @@ const dbName = process.env.DATABASE_NAME
 const uri = dbURI || 'mongodb://localhost:27017' // cambiar en prod
 const client = new MongoClient(uri)
 
+// evita que 
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined
+}
+
+if (!global._mongoClientPromise) {
+  const client = new MongoClient(uri)
+  global._mongoClientPromise = client.connect()
+}
+
 export const connectDatabase = async (): Promise<Db> => {
   await client.connect()
   const db = client.db(dbName)
