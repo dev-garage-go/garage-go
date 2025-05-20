4814554aca8961b16767de1f63fde0a5ad090bc4
Alex Arraga <arraga.alex@gmail.com>
2025-05-20 16:14:51 -0300

    wip

--- src/interfaces/shared/services.ts
+++ src/interfaces/shared/services.ts
@@ -2,16 +2,8 @@ import { Promotion } from './promotions';
 
 // Special options of services
 export type VehicleMileages = (
-  '10.000 kms' |
-  '20.000 kms' |
-  '30.000 kms' |
-  '40.000 kms' |
-  '50.000 kms' |
-  '60.000 kms' |
-  '70.000 kms' |
-  '80.000 kms' |
-  '90.000 kms' |
-  'Otro'
+  '10.000 kms' | '20.000 kms' | '30.000 kms' | '40.000 kms' | '50.000 kms' |
+  '60.000 kms' | '70.000 kms' | '80.000 kms' | '90.000 kms' | 'Otro'
 )
 
 export type QuantityTires = 1 | 2 | 3 | 4
