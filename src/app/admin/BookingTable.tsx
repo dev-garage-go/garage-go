3dd2a32df5d3f3914c530bc43ef07872725e6c22
Alex Arraga <arraga.alex@gmail.com>
2025-05-20 20:12:47 -0300

    wip

--- src/app/admin/BookingTable.tsx
+++ src/app/admin/BookingTable.tsx
@@ -1,12 +1,20 @@
 'use client'
 
 import { BookingDB } from "@/database/interfaces/bookings"
+import dayjs from 'dayjs';
 
 interface Props {
   bookings: BookingDB[]
 }
 
 export const BookingTable = ({ bookings }: Props) => {
+  if (!bookings) return;
+
+  const formatDate = (data: any): string => {
+    const f = dayjs(data).format('DD/MM/YYYY')
+    return f
+  }
+
   return (
     <table className="min-w-full">
       <thead className="bg-neutral-100 border-b">
@@ -36,46 +44,53 @@ export const BookingTable = ({ bookings }: Props) => {
       </thead>
 
       <tbody>
-        {bookings.map((booking) => (
-          <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-customGray-50 cursor-pointer">
+        {bookings.map((booking) => {
+          if (!booking) return;
 
-            <td className="table-row-style whitespace-nowrap">
-              {booking._id}
-            </td>
+          return (
+            <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-customGray-50 cursor-pointer">
 
-            <td className="table-row-style whitespace-nowrap">
-              {booking.user.name + " " + booking.user.lastName}
-            </td>
+              <td className="table-row-style whitespace-nowrap">
+                {booking._id}
+              </td>
 
-            <td className="table-row-style whitespace-nowrap">
-              {booking.service.name}
-            </td>
+              <td className="table-row-style whitespace-nowrap">
+                {booking.user.name + " " + booking.user.lastName}
+              </td>
 
-            <td className="table-row-style whitespace-nowrap">
-              {booking.appointment.date + " - " + booking.appointment.time}
-            </td>
+              <td className="table-row-style whitespace-nowrap">
+                {booking.service.name}
+              </td>
 
-            <td className="table-row-style whitespace-nowrap">
-              {booking.vehicle.brand + " " + booking.vehicle.model + " - " + booking.vehicle.year}
-            </td>
+              <td className="table-row-style whitespace-nowrap">
+                <div>
+                  <p>{booking.appointment?.date ? formatDate(booking.appointment.date) : 'sin fecha'}</p>
+                  <p>{booking.appointment?.time || 'sin hora'}</p>
+                </div>
+              </td>
 
-            <td className="table-row-style">
-              <div className="flex flex-col items-center justify-center gap-1">
-                <span>
-                  {booking.user.phone}
-                </span>
-                <span>
-                  {booking.user.email}
-                </span>
-              </div>
-            </td>
+              <td className="table-row-style whitespace-nowrap capitalize">
+                {booking.vehicle.brand + " " + booking.vehicle.model + " - " + booking.vehicle.year}
+              </td>
 
-            <td className="table-row-style max-w-40 text-wrap">
-              {booking.user.address}
-            </td>
+              <td className="table-row-style">
+                <div className="flex flex-col items-center justify-center gap-1">
+                  <span>
+                    {booking.user.phone}
+                  </span>
+                  <span>
+                    {booking.user.email}
+                  </span>
+                </div>
+              </td>
 
-          </tr>
-        ))}
+              <td className="table-row-style max-w-40 text-wrap">
+                {booking.user.address}
+              </td>
+
+            </tr>
+          )
+        })}
       </tbody>
     </table>
   )
