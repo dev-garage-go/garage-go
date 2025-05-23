bb9556220e19c4d602ea7f37a7f1bc69875cf792
Alex Arraga <arraga.alex@gmail.com>
2025-05-23 18:25:54 -0300

    admin page added

---
+++ src/app/admin/bookings/page.tsx
@@ -0,0 +1,17 @@
+import { getBookings } from "@/actions";
+import { BookingTable } from "../ui/BookingTable";
+import { BookingDB } from "@/database/interfaces";
+
+export default async function BookingAdminPage() {
+  const bookings = await getBookings() as BookingDB[]
+
+  return (
+    <section className="min-h-screen w-full bg-customGray-200 p-10">
+      <h1 className="title-h3 mb-6 font-semibold">
+        Reservas
+      </h1>
+
+      <BookingTable bookings={bookings} />
+    </section>
+  );
+}
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,17 +1,77 @@
-import { getBookings } from "@/actions";
-import { BookingTable } from "./ui/BookingTable";
-import { BookingDB } from "@/database/interfaces";
+"use client"
 
-export default async function AdminPage() {
-  const bookings = await getBookings() as BookingDB[]
+import { ErrorMessage } from "@/components";
+import { isAuthorized } from "@/keys";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function AdminPage() {
+  const router = useRouter()
+  const adminPasswordSecret = process.env.ADMIN_SECRET_PASSWORD
+
+  const [password, setPassword] = useState("")
+  const [wrongPassword, setWrongPassword] = useState<boolean>(false)
+  const [authorized, setAuthorized] = useState<boolean>(false);
+
+  useEffect(() => {
+    const hasAuthorized = sessionStorage.getItem(isAuthorized)
+    if (authorized || hasAuthorized) {
+      router.push("/admin/bookings")
+    }
+  }, [authorized])
+
+  const handleSumbit = () => {
+    if (password === adminPasswordSecret) {
+      sessionStorage.setItem(isAuthorized, "true")
+      setAuthorized(true)
+
+    } else {
+      setWrongPassword(true)
+    }
+  }
 
   return (
-    <section className="min-h-screen w-full bg-customGray-200 p-10">
-      <h1 className="title-h3 mb-6 font-semibold">
-        Reservas
-      </h1>
+    <div className="flex flex-col justify-center items-center bg-primaryBlue-50 min-h-screen">
+      <div className="flex flex-col justify-center items-center w-full max-w-xl px-6 py-32 bg-customGray-50 rounded-2xl">
+        <h2 className="title-h2 mb-10 font-medium text-center">
+          Panel de
+          <br />
+          Administración
+        </h2>
 
-      <BookingTable bookings={bookings} />
-    </section>
+        <p className="mb-10 text-center max-w-md">
+          Introduzca la contraseña de administrador para poder acceder al panel
+        </p>
+
+        <form
+          onSubmit={handleSumbit}
+          className="flex flex-col justify-start items-start w-full max-w-md"
+        >
+          <label
+            htmlFor="admin-pass"
+            className="text-sm text-customGray-500 font-light ml-2"
+          >
+            Contraseña
+          </label>
+          <div className="flex justify-center items-center gap-2 w-full">
+            <input
+              id="admin-pass"
+              type="password"
+              placeholder="******"
+              className="input-form"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="primary-button max-w-32"
+            >
+              Continuar
+            </button>
+          </div>
+          {wrongPassword && (<ErrorMessage message="Contraseña incorrecta, acceso denegado" className="pt-2" />)}
+        </form>
+      </div>
+    </div>
   );
 }
