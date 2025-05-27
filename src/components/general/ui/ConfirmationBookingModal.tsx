898454dc96dcc2600b87d61f083ae09b9b918f5b
Alex Arraga <arraga.alex@gmail.com>
2025-05-27 09:49:42 -0300

    confirmation modal improved

--- src/components/general/ui/ConfirmationBookingModal.tsx
+++ src/components/general/ui/ConfirmationBookingModal.tsx
@@ -10,19 +10,20 @@ interface Props {
 export const ConfirmationBookingModal = ({ success }: Props) => {
   return (
     <div className="fixed z-10 top-0 left-0 flex justify-center items-center w-screen h-full min-h-screen bg-primaryBlue-50 bg-opacity-90">
-      <div className="flex flex-col justify-center items-center bg-customGray-100 p-4 md:p-6 xl:p-10 rounded-2xl w-full h-full max-w-xl max-h-72 bg-opacity-100 shadow-lg shadow-customGray-400 mx-4">
+      <div className="flex flex-col justify-center items-center bg-customGray-100 p-4 md:p-6 xl:p-10 rounded-2xl w-full h-full max-w-2xl max-h-96 bg-opacity-100 shadow-lg shadow-customGray-400 mx-4">
 
         {success ? (
           <>
-            <div className="flex justify-start items-center">
+            <div className="flex flex-col lg:flex-row gap-2 lg:gap-0 justify-start items-center">
               <IoCheckmarkCircle className="text-secundaryGreen-500 w-8 h-8" />
-              <h2 className="title-h2 font-medium">¡Tu reserva ha sido confirmada!</h2>
+              <h2 className="title-h2 text-center lg:text-start font-medium">¡Tu reserva ha sido confirmada!</h2>
             </div>
 
             <div className="w-full max-w-lg mt-6">
-              <p className="text-center">Te mandamos un email a tu correo electronico confirmando todos los datos del servicio que contrataste
+              <p className="text-sm md:text-base text-center">Te mandamos un email a tu correo electronico confirmando todos los datos del servicio que contrataste
               </p>
-              <p className="text-center mt-4 font-medium text-primaryBlue-300">¡Gracias por confiar en nosotros!
+              <p className="text-sm md:text-base text-center mt-4 font-medium text-primaryBlue-300">
+                ¡Gracias por confiar en nosotros!
               </p>
 
               <div className="flex justify-center items-center w-full mt-6">
@@ -37,13 +38,14 @@ export const ConfirmationBookingModal = ({ success }: Props) => {
           </>
         ) : (
           <>
-            <div className="flex justify-start items-center">
+            <div className="flex flex-col lg:flex-row gap-2 lg:gap-0 justify-start items-center">
               <IoWarning className="text-yellow-600 w-8 h-8" />
               <h2 className="title-h2 font-medium">Oops... Algo salió mal</h2>
             </div>
 
             <div className="w-full max-w-lg mt-6">
-              <p className="text-center">Lamentamos los inconvenientes, le recomendamos que vuelva a intentar hacer su reserva o pongase en contacto con nosotros
+              <p className="text-sm md:text-base text-center">
+                Lamentamos los inconvenientes, le recomendamos que vuelva a intentar hacer su reserva o pongase en contacto con nosotros
               </p>
 
 
