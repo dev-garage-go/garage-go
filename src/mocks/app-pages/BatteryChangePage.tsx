0dfee023d2ecda2a2c8c390454d49fe3c287c6df
Alex Arraga <arraga.alex@gmail.com>
2025-05-29 03:33:47 -0300

    mocks folder deleted

--- src/mocks/app-pages/BatteryChangePage.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function BatteryChangePage() {
-  return (
-    <div>
-      <h1>Hello Page</h1>
-    </div>
-  );
-}
--- src/mocks/backend-actions/getServiceBySlug.ts
+++
@@ -1,8 +0,0 @@
-interface Service {
-  name: string,
-  slug: string,
-}
-
-export const getServiceBySlug = (serviceSlug: string): Service => {
-  return { slug: "algo", name: "nombre de servicio" }
-}
