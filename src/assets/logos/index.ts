8226671aed2554ba1f25f9d66a311140b168cae9
Alex Arraga <arraga.alex@gmail.com>
2025-05-29 19:57:10 -0300

    change name of logos file

--- src/assets/logos/index.ts
+++ src/assets/logos/index.ts
@@ -1,1 +1,19 @@
-export * from './logos';
+import { pathImages } from "../paths";
+
+export const logos = {
+  socialNetworks: {
+    facebook: `${pathImages}/icono_facebook.svg`,
+    instagram: `${pathImages}/icono_instagram.svg`,
+  },
+  general: {
+    whitePhone: `${pathImages}/logos_pagos.svg`,
+  },
+  company: {
+    logo: `${pathImages}/garage_go_logo_1.svg`,
+    logoFooter: `${pathImages}/garage_go_logo_2.svg`,
+    logoMetadata: `${pathImages}/garage-go-metadata-bg.png`
+  },
+  payment: {
+    methods: `${pathImages}/logos_pagos.svg`,
+  }
+}
