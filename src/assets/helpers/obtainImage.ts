756c4f6b7cc01ffcae1bda9d93ba1e9c34c47bf4
Alex Arraga <arraga.alex@gmail.com>
2025-05-29 17:33:03 -0300

    obainImage helper function added

---
+++ src/assets/helpers/obtainImage.ts
@@ -0,0 +1,15 @@
+import { assets } from "../images/component-images"
+
+export type AssetCategory = keyof typeof assets
+export type AssetKey<Category extends AssetCategory> = keyof typeof assets[Category]
+
+// obtain the image with the 'category' and the 'key' of the asset object
+export const obtainImage = <
+  C extends keyof typeof assets,
+  K extends keyof (typeof assets)[C]
+>(
+  category: C,
+  key: K
+): (typeof assets)[C][K] => {
+  return assets[category][key]
+}
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
+export * from './helpers/obtainImage';
 export * from './images/component-images';
 export * from './logos/company-logos';
 export * from './logos/social-networks';
