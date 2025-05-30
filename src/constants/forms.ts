49e49d047ca24943a119e64a1da6d43b2f1a3195
Alex Arraga <arraga.alex@gmail.com>
2025-05-30 16:59:29 -0300

    features/services/constants folder added

--- src/constants/forms.ts
+++ src/constants/forms.ts
@@ -1,14 +1,7 @@
 import { SelectOptions } from "@/components";
-import { TypesTiresOptions } from "@/features/services";
 
 export const AddressTypes: SelectOptions[] = [
   { id: 1, value: 'casa' },
   { id: 2, value: 'oficina' },
   { id: 3, value: 'depto' }
-]
-
-export const TypesTires: { label: string; value: TypesTiresOptions }[] = [
-  { label: 'Ciudad', value: 'ciudad' },
-  { label: 'Offroad', value: 'offroad' },
-  { label: 'Intermedio', value: 'intermedio' }
 ]
---
+++ src/features/services/constants/tires-change.ts
@@ -0,0 +1,7 @@
+import { TypesTiresOptions } from "../interfaces/services-data";
+
+export const TypesTires: { label: string; value: TypesTiresOptions }[] = [
+  { label: 'Ciudad', value: 'ciudad' },
+  { label: 'Offroad', value: 'offroad' },
+  { label: 'Intermedio', value: 'intermedio' }
+]
