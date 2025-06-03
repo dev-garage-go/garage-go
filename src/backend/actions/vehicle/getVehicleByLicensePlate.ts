b4554abceba584e3421d564aeb5f2d8e207592f0
Alex Arraga <arraga.alex@gmail.com>
2025-06-03 18:19:12 -0300

    getVehicleByLicensePlate ready

--- src/backend/actions/vehicle/getVehicleByLicensePlate.ts
+++ src/backend/actions/vehicle/getVehicleByLicensePlate.ts
@@ -1,24 +1,15 @@
 "use server"
 
+import { getCollection } from "@/backend/database"
 import { VehicleDataInterface } from "@/features/vehicle"
-import { sleep } from "@/utils"
 
 export const getVehicleByLicensePlate = async (licencePlate: string): Promise<VehicleDataInterface | null> => {
   try {
-    const vehicle = "algo"
-    // const vehicle = db.getVehicle(licensePlate)
+    const coll = await getCollection("vehicles")
+    const vehicle = await coll.findOne({ licensePlate: licencePlate })
+
     if (!vehicle) return null
-
-    sleep(400)
-    return {
-      brand: "Audi",
-      model: "A6 Turbo",
-      licensePlate: "ABC123",
-      mileage: "80000",
-      year: "2015"
-    }
-
-    // return null
+    return vehicle
 
   } catch (error) {
     console.error(error)
