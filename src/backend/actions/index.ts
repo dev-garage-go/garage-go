cd5f66b3b3ec942e11a0b3f888b092f572b83e81
Alex Arraga <arraga.alex@gmail.com>
2025-06-03 00:18:27 -0300

    actions exports fixed

--- src/backend/actions/index.ts
+++ src/backend/actions/index.ts
@@ -1,8 +1,13 @@
-export * from './admin/validateAdminPassword';
-export * from './booking/createBooking';
-export * from './booking/getBookings';
-export * from './emails/send-email';
-export * from './services/calcMileageMaintenanceAmount';
-export * from './services/calcTiresChangeAmount';
-export * from './services/getServicesAmount';
-export * from './vehicle/getVehicleByLicensePlata';
+/*
+Actions cannot be exported by default because next.js will generate a conflict. This file must contain only exports named
+Example: export { functionExample } from './example-file'.
+*/
+
+export { validateAdminPassword } from './admin/validateAdminPassword';
+export { createBooking } from './booking/createBooking';
+export { getBookings } from './booking/getBookings';
+export { sendEmailAction } from './emails/send-email';
+export { calcMileageMaintenanceAmount } from './services/calcMileageMaintenanceAmount';
+export { calcTiresChangeAmount } from './services/calcTiresChangeAmount';
+export { getServiceAmount } from './services/getServicesAmount';
+export { getVehicleByLicensePlate } from './vehicle/getVehicleByLicensePlata';
