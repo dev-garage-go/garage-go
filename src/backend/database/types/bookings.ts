ad11697a5febbee540f6f4f8845a1aa55bd29e13
Alex Arraga <arraga.alex@gmail.com>
2025-06-04 17:55:20 -0300

    wip

--- src/backend/database/types/bookings.ts
+++ src/backend/database/types/bookings.ts
@@ -5,13 +5,14 @@ import { VehicleWithStringIDInterface } from "@/features/vehicle"
 
 // objeto de como queda la reserva en base de datos, con el vehicle id siendo un ObjectID de Mongo
 export interface BookingDB extends Omit<BookingServiceDataInterface, 'vehicleID'> {
-  _id?: string
+  _id?: ObjectId  // debe ser opcional porque de esa forma mongodb se encarga de crearlo al impactar la db
   price?: number
   vehicleID: ObjectId
 }
 
 // objeto de como deberian ver los vendedores una reserva, ignorando el vehicleID 
 // y viendo el vehiculo
-export interface BookingAdmin extends Omit<BookingDB, 'vehicleID'> {
+export interface BookingAdmin extends Omit<BookingDB, 'vehicleID' | '_id'> {
+  _id: string
   vehicle: VehicleWithStringIDInterface
 }
