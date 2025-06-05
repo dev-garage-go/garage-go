e537a30097286194a568f268ed55dc58528eb436
Alex Arraga <arraga.alex@gmail.com>
2025-06-05 16:36:01 -0300

    createBooking improved

--- src/backend/actions/booking/createBooking.ts
+++ src/backend/actions/booking/createBooking.ts
@@ -5,9 +5,10 @@ import { HttpStatus, ServerActionResponse } from "@/backend/types";
 import { BookingServiceDataInterface } from "@/features/bookings";
 import { ObjectId } from "mongodb";
 
-export const createBooking = async (booking: BookingServiceDataInterface): Promise<ServerActionResponse<void>> => {
+export const createBooking = async (booking: BookingServiceDataInterface): Promise<ServerActionResponse<BookingDB>> => {
   try {
     const coll = await getCollection("bookings")
+    if (!coll) throw new Error("error getting bookings collection")
 
     // Convert the vehicle id in a mongo ObjectId
     const { vehicleID, ...rest } = booking
@@ -18,12 +19,16 @@ export const createBooking = async (booking: BookingServiceDataInterface): Promi
       ...rest
     }
 
-    coll.insertOne(validBooking)
+    const result = await coll.insertOne(validBooking)
+    if (!result) throw new Error("error creating a booking")
+
+    const bookingCreated = await coll.findOne({ _id: result.insertedId })
+    if (!bookingCreated) throw new Error("error getting the booking created")
 
     return {
       success: true,
       httpStatus: HttpStatus.OK,
-      data: null
+      data: bookingCreated
     }
 
   } catch (error) {
