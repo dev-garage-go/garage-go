19c6ebb86356057837566f3a3a5ce31da9faf606
Alex Arraga <arraga.alex@gmail.com>
2025-06-17 00:39:38 -0300

    cypress config updated

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-    viewportHeight: 1000,
     setupNodeEvents(on, config) {
       require("cypress-localstorage-commands/plugin")(on, config);
       return config;
