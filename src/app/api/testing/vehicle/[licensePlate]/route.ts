fcbde182105dfbdd4fcd32444a503cc8ad4af7c5
Alex Arraga <arraga.alex@gmail.com>
2025-06-16 22:15:38 -0300

    new dynamic api route

---
+++ src/app/api/testing/vehicle/[licensePlate]/route.ts
@@ -0,0 +1,29 @@
+import { findVehicleByLicensePlate } from "@/backend/database/queries"
+import { NextResponse } from "next/server"
+
+interface Params {
+  params: { licensePlate: string }
+}
+
+// find a vehicle in db
+export async function GET(_: Request, { params }: Params) {
+  if (process.env.NODE_ENV !== "development") {
+    return NextResponse.json({ error: "Not allowed in production" }, { status: 403 })
+  }
+
+  try {
+    const result = await findVehicleByLicensePlate(params.licensePlate)
+
+    if (!result.success) {
+      return NextResponse.json({ error: result.error }, { status: 500 })
+    }
+
+    if (!result.data) {
+      return NextResponse.json({ found: false }, { status: 200 })
+    }
+
+    return NextResponse.json({ found: true, vehicle: result.data }, { status: 200 })
+  } catch (error) {
+    return NextResponse.json({ error: "Unexpected server error" }, { status: 500 })
+  }
+}
