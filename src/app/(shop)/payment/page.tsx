4a54b7d23d329ac33a40e2c660aa7d8f846b9ae0
Alex Arraga <arraga.alex@gmail.com>
2025-06-25 16:33:05 -0300

    guards added

--- src/app/(shop)/payment/page.tsx
+++ src/app/(shop)/payment/page.tsx
@@ -1,7 +1,12 @@
 import { TopBanner } from "@/features/home";
 import { PaymentFormWrapper } from "./ui";
+import { redirect } from "next/navigation";
+
 
 export default function PaymentPage() {
+  // guard
+  redirect("/")
+
   return (
     <div>
       <TopBanner hasVehicleData />
--- src/app/(shop)/services/tires_change/contracting/page.tsx
+++ src/app/(shop)/services/tires_change/contracting/page.tsx
@@ -1,7 +1,11 @@
 import { TopBanner } from "@/features/home";
 import { TiresContractingWrapper } from "./ui";
+import { redirect } from "next/navigation";
 
 export default function TiresCheckoutPage() {
+  // guard
+  redirect("/services")
+
   return (
     <div>
       <TopBanner hasVehicleData />
