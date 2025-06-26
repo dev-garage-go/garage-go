b02719b1f41a5003f05ca3eb1a52569867acdd03
dev-garage-go <development@garageservice.cl>
2025-06-26 12:37:08 -0300

    toast added

--- src/providers/ContextsProvider.tsx
+++ src/providers/ContextsProvider.tsx
@@ -9,6 +9,7 @@ import {
   PaymentContextProvider
 } from "./contexts"
 import { RefreshListener } from "./RefreshListener"
+import { Toaster } from 'sonner';
 
 interface Props {
   children: React.ReactNode
@@ -17,7 +18,13 @@ interface Props {
 export const ContextsProvider = ({ children }: Props) => {
   return (
     <>
+      <Toaster
+        richColors
+        position="bottom-center"
+      />
       <RefreshListener />
+
+      {/* Apps contexts */}
       <VehicleContextProvider>
         <ServiceContextProvider>
           <EmailContextProvider>
