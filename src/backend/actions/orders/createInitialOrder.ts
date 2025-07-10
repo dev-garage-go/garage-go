6828c8f5c5d1c4a88a725623d184f6e72635185b
Alex Arraga <arraga.alex@gmail.com>
2025-07-10 19:13:21 -0300

    createInitialOrder action added

---
+++ src/backend/actions/orders/createInitialOrder.ts
@@ -0,0 +1,48 @@
+"use server"
+
+import { AmountInterface } from "@/features/bookings"
+import { getBaseAmountInCookie } from "../payment/cookies/amount"
+import { getBookingByID } from "../booking/getBookingByID"
+import { InitialOrderType, ProvidersType } from "@/features/orders"
+import { insertOrder } from "@/backend/database/queries"
+import { HttpStatus, ServerActionResponse } from "@/backend/types"
+
+interface PayloadInitialOrder {
+  bookingId: string,
+  provider: ProvidersType
+}
+
+export const createInitialOrder = async ({ bookingId, provider }: PayloadInitialOrder): Promise<ServerActionResponse<InitialOrderType>> => {
+  const amountResponse = await getBaseAmountInCookie()
+  if (!amountResponse.success || !amountResponse.data) throw new Error(amountResponse.error)
+
+  const amount = amountResponse.data
+
+  const bookingResponse = await getBookingByID(bookingId)
+  if (!bookingResponse.success || !bookingResponse.data) throw new Error(bookingResponse.error)
+
+  const booking = bookingResponse.data
+  if (!booking._id) throw new Error("couldn't find the booking id")
+
+  const initialOrder: InitialOrderType = {
+    provider: provider,                           // mercado-pago | getnet | webpay
+    email: booking.user.email,
+    booking_id: booking._id.toString(),           // payload.booking.id
+    external_reference: booking.service.type,     // service type
+    total_price: amount.total,                    // payload.total_price
+    pay_status: 'pending',
+    created_at: new Date().toISOString(),
+    updated_at: new Date().toISOString(),
+    expires_at: new Date(Date.now() + 1000 * 60 * 60).toISOString() // TTL = 1h
+  }
+
+  const result = await insertOrder(initialOrder)
+  if (!result.success || !result.data) throw new Error(result.error)
+  const order = result.data
+
+  return {
+    success: true,
+    httpStatus: HttpStatus.OK,
+    data: order
+  }
+}
