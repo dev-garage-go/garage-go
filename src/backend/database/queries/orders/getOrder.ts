a421feee74f67f1728b6d409d3a2e4ab23ff731e
Alex Arraga <arraga.alex@gmail.com>
2025-07-16 16:27:21 -0300

    getOrder added

---
+++ src/backend/database/queries/orders/getOrder.ts
@@ -0,0 +1,40 @@
+import { ObjectId } from "mongodb"
+import { getCollection } from "@/backend/database/methods"
+import { OrderServerResponseType } from "@/backend/database/schemas"
+import { HttpStatus, ServerActionResponse } from "@/backend/types"
+
+import { zObjectIdSchema } from "@/utils/zod-helpers"
+
+export const getOrderByID = async (id: string): Promise<ServerActionResponse<OrderServerResponseType>> => {
+  try {
+    const checkId = zObjectIdSchema.safeParse(id)
+    if (!checkId.success) throw checkId.error
+    const validId = new ObjectId(id)
+
+    const coll = await getCollection("orders")
+    const order = await coll.findOne({ _id: validId })
+
+    if (!order) throw new Error(`the order with id ${id} doesn't exist`)
+    const { _id, booking_id, ...rest } = order
+
+    const response: OrderServerResponseType = {
+      _id: _id.toString(),
+      booking_id: booking_id.toString(),
+      ...rest
+    }
+
+    return {
+      success: true,
+      httpStatus: HttpStatus.OK,
+      data: response
+    }
+
+  } catch (error) {
+    console.error(error)
+    return {
+      success: false,
+      httpStatus: HttpStatus.INTERNAL_SERVER_ERROR,
+      error: error as string
+    }
+  }
+}
