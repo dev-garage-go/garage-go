aa7ff0761393c9cb28411a59a65913206e973dd1
Alex Arraga <arraga.alex@gmail.com>
2025-07-22 01:09:36 -0300

    tokenization: credit/debit card number

--- src/features/payment/schemas/getnet.ts
+++ src/features/payment/schemas/getnet.ts
@@ -1,4 +1,5 @@
 import { z } from "zod"
+import { zObjectIdSchema } from "@/utils/zod-helpers"
 
 export const GetnetAuthTokenSchema = z.object({
   token_type: z.literal("Bearer"),
@@ -7,3 +8,16 @@ export const GetnetAuthTokenSchema = z.object({
   refresh_token: z.string()
 })
 export type GetnetAuthTokenType = z.infer<typeof GetnetAuthTokenSchema>
+
+// Tokenization
+export const GetnetTokenizationRequestSchema = z.object({
+  user_card_number: z.string(),
+  user_email: z.string().email(),
+  user_order_id: zObjectIdSchema
+})
+export type GetnetTokenizationRequestType = z.infer<typeof GetnetTokenizationRequestSchema>
+
+export const TokenReturnedSchema = z.object({
+  number_token: z.string()
+})
+export type TokenReturnedType = z.infer<typeof TokenReturnedSchema>
