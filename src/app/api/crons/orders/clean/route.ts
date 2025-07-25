7841b7e2419130d008ac2e7bfa82fdc5a6142515
Alex Arraga <arraga.alex@gmail.com>
2025-07-25 17:20:51 -0300

    wip

---
+++ src/app/api/crons/orders/clean/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server"
+import { HttpStatus } from "@/backend/types"
+import { purgeSoftDeleteOrders } from "@/backend/actions"
+
+export async function GET(): Promise<NextResponse> {
+  try {
+    const result = await purgeSoftDeleteOrders()
+    if (!result.success || !result.data) throw result.error
+
+    return NextResponse.json({
+      message: result.data
+    }, { status: result.httpStatus })
+
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json({ error: error }, { status: HttpStatus.INTERNAL_SERVER_ERROR })
+  }
+}
